0346fa105095bffb2faba488aa975e43c73ed410
Yanis Dib <ynsprive@gmail.com>
2022-11-06 23:02:04 +0100

    Create useResizableDimensions hooks

---
+++ src/hooks/useResizableDimensions.ts
@@ -0,0 +1,30 @@
+import {
+    useEffect,
+    useState
+} from 'react';
+
+
+type ReturnValue = {
+    width: number,
+    height: number
+}
+
+
+export const useResizableDimensions = (ref: React.MutableRefObject<any>): ReturnValue => {
+    const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+    const updateDimensions = () => {
+        setDimensions({
+            width: ref.current.offsetWidth,
+            height: ref.current.offsetHeight,
+        });
+    }
+
+
+    useEffect(() => {
+        updateDimensions();
+        window.addEventListener('resize', updateDimensions, false);
+    }, [ref]);
+
+    return dimensions;
+}
