881cb236a9f79871749f67014e5057b1fdd184f0
Yanis Dib <ynsprive@gmail.com>
2022-11-06 23:01:15 +0100

    Create Container component

---
+++ src/components/Container/Container.styled.ts
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+
+//TODO: Edit values after UI tests
+
+export default styled.div`
+    --container-max-width-xs: 100vw;
+    --container-max-width-sm: 100vw;
+    --container-max-width-md: 960px;
+    --container-max-width-lg: 1360px;
+    --container-max-width-xl: 1470px;
+    --container-padding-x: 80px !important;
+
+    @media (min-width: 375px) {
+        display: flex;
+        justify-content: center;
+        width: 95vw;
+        max-width: var(--container-max-width-xs);
+        padding-left: 0;
+        padding-right: 0;
+        margin: 50px auto;
+    }
+
+    @media (min-width: 744px) {
+        display: flex;
+        width: 95vw;
+        max-width: var(--container-max-width-sm);
+        padding-left: 0;
+        padding-right: 0;
+    }
+
+    @media (min-width: 950px) {
+        display: flex;
+        width: 84vw;
+        max-width: var(--container-max-width-md);
+    }
+
+    @media (min-width: 1128px) {
+        display: flex;
+        width: 84vw;
+        max-width: var(--container-max-width-lg);
+    }
+    
+    @media (min-width: 1440px) {
+        display: flex;
+        width: 95vw;
+        max-width: var(--container-max-width-xl);
+        padding-left: var(--container-padding-x);
+        padding-right: var(--container-padding-x);
+    }
+`;
