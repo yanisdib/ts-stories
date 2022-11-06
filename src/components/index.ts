59a1942fd4cb2091d2424aaa2e83d47a36553cbe
Yanis Dib <ynsprive@gmail.com>
2022-11-06 23:01:48 +0100

    Update components exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,6 @@
-export {default as Loader } from './Loader/Loader';
+export { default as Button } from './Button/Button';
+export { default as Container } from './Container/Container.styled';
+export { default as Loader } from './Loader/Loader';
 export { default as Tag } from './Tag/Tag';
+
+export { default as Carousel } from './Carousel/Carousel';
